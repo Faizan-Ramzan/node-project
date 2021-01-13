7e21f6ff1e5e994ada4e1d9fe49032507063eced
faizan-invozone <faizanramzan0011@gmail.com>
2021-01-13 19:04:04 +0500

    added code in some files

--- index.js
+++ index.js
@@ -2,60 +2,70 @@ var os = require('os');
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
-const homeDir = os.homedir()+process.env.DIRECTORY_PATH;
+const homeDir = os.homedir() + process.env.DIRECTORY_PATH;
 // console.log(homeDir);
 
-const pathWithName = homeDir + "operating-system-info.txt";
+const pathWithName = homeDir + 'operating-system-info.txt';
 
 const operatingSystemInfo = () => {
-    let str = `Platform : ${os.platform()}\nArchitecture: ${os.arch()}\nNo of CPU's: ${os.cpus().length}\nUptime of operating systems in Seconds: ${os.uptime()}\nUser Information: ${os.userInfo().username}`;
+  let str = `Platform : ${os.platform()}\nArchitecture: ${os.arch()}\nNo of CPU's: ${
+    os.cpus().length
+  }\nUptime of operating systems in Seconds: ${os.uptime()}\nUser Information: ${
+    os.userInfo().username
+  }`;
 
-    return str;
-}
+  return str;
+};
 
 let dirPromise = new Promise((resolve, reject) => {
-    fs.mkdir(homeDir, { recursive: true }, (err) => {
-        if (err) {
-            reject(err);
-        }else{
-            resolve("Directory is created.");
-        }
-    });
+  fs.mkdir(homeDir, { recursive: true }, (err) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve('Directory is created.');
+    }
+  });
 });
+
 let fileWritePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        fs.writeFile(pathWithName, operatingSystemInfo(), (err) => {
-            if (err){
-                reject(err);
-            }else{
-                resolve("File is created.")
-            }
-        });
-    }, 500);
+  setTimeout(() => {
+    fs.writeFile(pathWithName, operatingSystemInfo(), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve('File is created.');
+      }
+    });
+  }, 500);
 });
 
 try {
-    if (fs.existsSync(pathWithName)) {
-        fileWritePromise.then((res)=>{
-            console.log("updated successfully");
-        }).catch(err=>{
-            console.log("not updated!");
-        });
-    }else{
-        dirPromise.then((res) => {
-            console.log(res);
-            fileWritePromise.then((fileRes)=>{
-                console.log(fileRes);
-            }).catch(comErr=>{
-                console.log("File not created!");
-            });
-        }).catch(err=>{
-            console.log(err);
-        });
-
-    }
-} catch(err) {
-    console.error(err)
+  if (fs.existsSync(pathWithName)) {
+    fileWritePromise
+      .then((res) => {
+        console.log('updated successfully');
+      })
+      .catch((err) => {
+        console.log('not updated!');
+      });
+  } else {
+    dirPromise
+      .then((res) => {
+        console.log(res);
+        fileWritePromise
+          .then((fileRes) => {
+            console.log(fileRes);
+          })
+          .catch((comErr) => {
+            console.log('File not created!');
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+} catch (err) {
+  console.error(err);
 }
 
 // console.log(process.env.DIRECTORY_PATH);
